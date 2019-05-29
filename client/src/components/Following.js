e712396e4825876c6b569a22185dc2f07d211a8e
jsunga <jawyn.sunga@gmail.com>
2019-05-28 23:56:16 -0700

    added following and follower list

--- client/src/components/Following.js
+++ client/src/components/Following.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
+import { Placeholder } from 'semantic-ui-react'
 import Navbar from './Navbar'
+import Summary from './Summary'
 import List from './List'
+import styled from 'styled-components'
 import axios from 'axios'
 
+const Container = styled.div`
+  width: 1210px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+`
+
+const ProfileWrapper = styled.div`
+  width: 305px;
+  margin-top: 15px;
+  height: 205px;
+`
+
+const PlaceholderWrapper = styled.div`
+  background-color: white;
+  margin-right: 10px;
+  height: 205px;
+  padding: 15px;
+`
+
+const ListWrapper = styled.div`
+  width: 905px;
+  margin-top: 15px;
+  overflow: auto;
+`
+
 export default class Following extends Component {
+
+  state = {
+    user_details: [],
+    isLoading: true,
+    users: [],
+  }
 
   componentDidMount() {
     const { handle } = this.props.match.params
@@ -13,9 +48,26 @@ export default class Following extends Component {
   getFollowing = handle => {
     axios.get(`/api/user/retrieve/${handle}`)
     .then(res => {
+      let data = res.data
+      data.firstname = data.firstname.charAt(0).toUpperCase() + data.firstname.slice(1)
+      data.lastname = data.lastname.charAt(0).toUpperCase() + data.lastname.slice(1)
+      this.setState({ user_details: res.data, isLoading: false })
       axios.get(`/api/follow/get_following/${res.data.user_id}`)
       .then(res => {
-        console.log(res.data)
+        let temp = []
+        res.data.forEach(item => {
+          axios.get(`/api/user/get/${item.following_user_id}`)
+          .then(res => {
+            let data = res.data
+            data.firstname = data.firstname.charAt(0).toUpperCase() + data.firstname.slice(1)
+            data.lastname = data.lastname.charAt(0).toUpperCase() + data.lastname.slice(1)
+            temp.push(data)
+            this.setState({ users: temp })
+          })
+          .catch(err => {
+            console.log(err)
+          })
+        })
       })
     })
     .catch(err => {
@@ -23,12 +75,41 @@ export default class Following extends Component {
     })
   }
 
+  getRender = () => {
+    if (this.state.isLoading === true) {
+      return (
+        <PlaceholderWrapper>
+        <Placeholder>
+          <Placeholder.Header image>
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Header>
+          <Placeholder.Paragraph>
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+            <Placeholder.Line />
+          </Placeholder.Paragraph>
+        </Placeholder>
+        </PlaceholderWrapper>
+      )
+    } else {
+      return (
+        <Summary {...this.state}/>
+      )
+    }
+  }
+
   render() {
     return (
-      <div>
-        <Navbar/>
-        <List/>
-      </div>
+      <>
+        <style>{'body { background-color: #e6ecf0; }'}</style>
+        <Navbar {...this.props}/>
+        <Container>
+          <ProfileWrapper>{this.getRender()}</ProfileWrapper>
+          <ListWrapper><List {...this.state}/></ListWrapper>
+        </Container>
+      </>
     )
   }
 }
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,11 +1,50 @@
 import React from 'react'
+import { withRouter, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  width: 215px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  display: inline-block;
+  background-color: white;
+`
+
+const Image = styled.img`
+  width: 215px;
+  height: 150px;
+`
+
+const Header = styled.div`
+  color: black;
+  font-weight: bold;
+  font-size: 17px;
+  padding-left: 10px;
+  padding-top: 5px;
+`
+
+const Username = styled.div`
+  color: grey;
+  padding-left: 10px;
+  padding-bottom: 10px;
+`
 
 const List = props => {
   return (
     <>
-    hello
+      {props.users.map(item => (
+        <Link to={`/user/${item.username}`} key={item.user_id}>
+        <Card>
+          <Image src={item.image_url} alt="thumbnail"/>
+          <div>
+            <Header>{item.firstname} {item.lastname}</Header>
+            <Username>@{item.username}</Username>
+          </div>
+        </Card>
+        </Link>
+      ))}
     </>
   )
 }
 
-export default List
+export default withRouter(List)
