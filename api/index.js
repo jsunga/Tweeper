2c7942fe570e61d9da53b9cdeecd0251f8d12b49
jsunga <jawyn.sunga@gmail.com>
2019-06-10 17:26:24 -0700

    added express path

--- api/index.js
+++ api/index.js
@@ -8,7 +8,9 @@ const sessionMiddleware = require('../config/session')
 
 const port = process.env.PORT || 5000
 const app = express()
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, '../client/build')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(sessionMiddleware)
